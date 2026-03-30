9e62f50242da7539a5c355da18ad01f584711618
Gale W <mail@galewilliams.com>
2026-03-30 12:37:47 -0400

    Initial Commit

---
+++ SayBarTests/SayBarTests.swift
@@ -0,0 +1,19 @@
+//
+//  SayBarTests.swift
+//  SayBarTests
+//
+//  Created by Gale Williams on 3/30/26.
+//
+
+import Testing
+@testable import SayBar
+
+struct SayBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
